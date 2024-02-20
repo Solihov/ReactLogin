5c671552f3ace54224a664a2138db9f29fd0d42b
Solihov <islomjonsolihov@gmail.com>
2024-02-20 12:22:24 +0500

    cont

--- CloudClient/src/App.jsx
+++ CloudClient/src/App.jsx
@@ -1,10 +1,20 @@
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route element={<LayoutCustom />}>
+        <Route index element={<DiscPage />} />
+        <Route path="*" element={<>404</>} />
+      </Route>
+    </Routes>
+  )
+  return (
+    <Routes>
+      <Route index element={<LoginPage />} />
+      <Route path="register" element={<RegisterPage />} />
+    </Routes>
   )
 }
+
 
 export default App
